b3a65641d902d489018afbf4516a9967b260d216
boloempire <boloempire@outlook.com>
2022-09-24 19:05:07 -0500

    bug fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').
-BundleAnalyzerPlugin
+
 
 module.exports = {
    mode: 'development',
@@ -54,6 +53,4 @@ module.exports = {
       filename: 'index.html',
       template: 'src/template.html',
     }),
-      new BundleAnalyzerPlugin(),
-   ],
-}
+  ],}
